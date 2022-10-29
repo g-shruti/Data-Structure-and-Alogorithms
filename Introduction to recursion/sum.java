aa4882985c8160a88ae89188de181b74195bae5f
Shruti Jain <jainshruti.rattan@gmail.com>
2022-10-29 09:35:50 +0530

    ..

---
+++ Introduction to recursion/sum.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class powerLog {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int x= scn.nextInt();
+        int y =scn.nextInt();
+        System.out.println(x+y);
+    }
+}
